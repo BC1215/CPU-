287868309a1b198780ebe0a99761cbbe0e7cd390
baichen <baichen@hexfuture.com>
2020-11-28 22:35:28 +0800

    十秒后自动退出

--- CPU使用率监控/Program.cs
+++ CPU使用率监控/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace CPU使用率监控
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("config.json", true, true)
@@ -14,12 +16,11 @@ namespace CPU使用率监控
             monitor.UsageUpdate += Monitor_UsageUpdate;
             monitor.Start();
 
-            Console.ReadLine();
+            await Task.Delay(10 * 1000);
         }
 
         private static void Monitor_UsageUpdate(System.Collections.Generic.List<(System.Diagnostics.Process Process, double Usage)> Usage)
         {
-            Console.Clear();
             foreach (var u in Usage)
             {
                 Console.WriteLine($"{u.Process.ProcessName}[{u.Process.Id}]:{u.Usage:P2}");
